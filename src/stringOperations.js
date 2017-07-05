4b651c71335a366d579dd51a15017779538b4f27
Matthias Jäggli <matthias.jaeggli@gmail.com>
2017-07-05 14:29:51 +0200

    accept empty strings

--- src/stringOperations.js
+++ src/stringOperations.js
@@ -2,10 +2,12 @@ const lowercaseWords = /(?:^\w|[A-Z]|\b\w)/g
 const wordSeparators = /[\s-:]+/g
 
 const camelCase = str => str
+  .trim()
   .replace(lowercaseWords, (letter, index) => index === 0 ? letter.toLowerCase() : letter.toUpperCase())
   .replace(wordSeparators, '')
 
 const pascalCase = str => {
+  if (!str) return str
   const camel = camelCase(str)
   return camel[0].toUpperCase() + camel.slice(1)
 }
