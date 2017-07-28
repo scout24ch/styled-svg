3fe61cd91a183124e5f021fbca65058b9d9aaed9
Matthias Jäggli <matthias.jaeggli@gmail.com>
2017-07-28 16:04:35 +0200

    add snapshot testing

--- src/templates/test.js
+++ src/templates/test.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 import Image from '{{FILENAME}}'
 
-const negativeNumberString = expect.stringMatching(/^-/)
-
 describe('{{NAME}}.svg generated styled component', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(<Image />)
+  })
+
   it('renders a <svg> tag without crashing', () => {
-    const wrapper = shallow(<Image />)
     expect(wrapper).toHaveTagName('svg')
+  })
+
+  it('renders correctly according to snapshot', () => {
+    expect(toJson(wrapper)).toMatchSnapshot()
   })
 
   it('has dimensions greater than zero', () => {
     const dimensions = Image.getDimensions()
     expect(dimensions.width).not.toBe('0')
-    expect(dimensions.width).not.toEqual(negativeNumberString)
+    expect(parseInt(dimensions.width, 10)).toBeGreaterThan(0)
     expect(dimensions.height).not.toBe('0')
-    expect(dimensions.height).not.toEqual(negativeNumberString)
+    expect(parseInt(dimensions.height, 10)).toBeGreaterThan(0)
   })
 })
